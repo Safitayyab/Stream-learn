9f3e4023784196e82185a7a4b2de8c2e7351693b
Mohd Safiuddin Tayyab <safitayyab2003@gmail.com>
2025-05-10 06:50:35 +0000

    initial commit

---
+++ backend/src/routes/user.route.js
@@ -0,0 +1,11 @@
+import express from 'express';
+import { protectRoute } from '../middleware/auth.middleware';
+
+const router = express.Router();
+router.use(protectRoute);
+
+router.get("/", getRecommendedUsers);
+router.get("/friends", getFriends);
+
+
+export default router;
---
+++ backend/src/server.js
@@ -0,0 +1,20 @@
+import express from "express";
+import dotenv from "dotenv";
+dotenv.config();
+import cookieParser from "cookie-parser";
+const app = express();
+const PORT = process.env.PORT;
+
+import authRoutes from "./routes/auth.route.js";
+import userRoutes from "./routes/user.route.js";
+import { connectDB } from "./lib/db.js";
+
+app.use(express.json());
+app.use(cookieParser());
+app.use("/api/auth", authRoutes)
+app.use("/api/users", userRoutes)
+
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+  connectDB();
+});
