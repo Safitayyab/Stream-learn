74720453801d2e4f4e6920009f79074c6ef2b02d
Mohd Safiuddin Tayyab <safitayyab2003@gmail.com>
2025-05-10 08:47:12 +0000

    Updated user controller and route

--- backend/src/controllers/user.controller.js
+++ backend/src/controllers/user.controller.js
@@ -1,3 +1,34 @@
-export async function getRecommendedUsers(req, res) {}
+import { User } from "../models/User.js";
+export async function getRecommendedUsers(req, res) {
+    try{
+        const currentUserId = req.user.id;
+        const currentUser = req.user;
 
-export async function getFriends(req, res) {}
+        const recommendedUser = await User.find({
+            $and: [
+                { _id: { $ne: currentUserId } },
+                { $id : {$nin: currentUser.friends} },
+                { isOnboarded: true },
+            ]
+        })
+        res.status(200).json(recommendedUser);
+
+
+    } catch (error) {
+        console.error("Error in getRecommendedUsers:", error.message);
+        res.status(500).json({ message: "Internal server error" });
+    }
+}
+
+export async function getMyFriends(req, res) {
+    try {
+      const user = await User.findById(req.user.id)
+        .select("friends")
+        .populate("friends", "fullName profilePic nativeLanguage learningLanguage");
+  
+      res.status(200).json(user.friends);
+    } catch (error) {
+      console.error("Error in getMyFriends controller", error.message);
+      res.status(500).json({ message: "Internal Server Error" });
+    }
+  }
--- backend/src/routes/user.route.js
+++ backend/src/routes/user.route.js
@@ -1,5 +1,6 @@
 import express from 'express';
 import { protectRoute } from '../middleware/auth.middleware';
+import { getRecommendedUsers, getFriends } from '../controllers/user.controller.js';
 
 const router = express.Router();
 router.use(protectRoute);
