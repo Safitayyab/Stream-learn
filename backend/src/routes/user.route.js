5e459bfcf59847d020f0a9453140142ce9a930aa
Mohd Safiuddin Tayyab <safitayyab2003@gmail.com>
2025-05-12 00:46:14 +0000

    update

--- backend/src/routes/user.route.js
+++ backend/src/routes/user.route.js
@@ -1,12 +1,14 @@
 import express from 'express';
 import { protectRoute } from '../middleware/auth.middleware';
-import { getRecommendedUsers, getFriends } from '../controllers/user.controller.js';
+import { getRecommendedUsers, getFriends, sendFriendRequest } from '../controllers/user.controller.js';
 
 const router = express.Router();
 router.use(protectRoute);
 
 router.get("/", getRecommendedUsers);
 router.get("/friends", getFriends);
+
+router.post("/friend-request/:id", sendFriendRequest);
 
 
 export default router;
